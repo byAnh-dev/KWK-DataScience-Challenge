e1417d9215f81c29629c81242eb9df4b4aa7153b
AnhHoang23 <154464393+AnhHoang23@users.noreply.github.com>
2025-07-07 09:51:16 -0400

    Update for publish

--- Website/svelte.config.js
+++ Website/svelte.config.js
@@ -1,18 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	extensions: ['.svelte'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
-	
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+const dev = process.env.NODE_ENV === 'development';
+const base = dev ? '' : '/KWK-DataScience-Challenge'; 
+
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base
+    }
+  },
+  preprocess: vitePreprocess()
 };
-export default config;
